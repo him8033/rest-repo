6f24147de062684dbe6aaaeadffda3501e694967
him8033 <himanshu8033@gmail.com>
2024-11-21 15:08:41 +0530

    install method-override package and send patch request through edit.ejs page using method-override package

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
@@ -48,6 +50,24 @@ app.get("/posts/:id", (req, res) => {
     let post = posts.find((p) => id === p.id);
     console.log(post);
     res.render("show.ejs", { post });
+})
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    console.log(id);
+    let newContent = req.body.content;
+    console.log(newContent);
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    console.log(post);
+    res.render("edit.ejs", { post });
 })
 
 app.listen(port, () => {
